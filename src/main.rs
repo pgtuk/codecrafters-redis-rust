94435fe58fbb8154d0ef2dd4c11ad2c8c3196548
unknown <gtuk@ukr.net>
2024-05-10 16:49:32 +0300

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("+PONG\r\n");
+            Ok(mut stream) => {
+                let response = "+PONG\r\n";
+                stream.write_all(response.as_bytes()).unwrap();
+                stream.flush().unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
