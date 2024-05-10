f3ae260f198f12d32bd93e1ea47a19715110ef4a
unknown <gtuk@ukr.net>
2024-05-10 18:02:44 +0300

    pass stage

--- src/redis.rs
+++ src/redis.rs
@@ -1,6 +1,6 @@
 use std::{
     io::prelude::*, 
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
 };
 
 
@@ -17,29 +17,33 @@ impl Redis {
     }
 
     pub fn run(&self) {
+        println!("RUN");
         for stream in self.listener.incoming() {
             match stream {
-                Ok(mut stream) => {
-                    let mut command = vec![];
-                    
-                    loop {
-                        let read = stream.read(&mut command);
-                        match read {
-                            Ok(0) => break,
-                            Ok(_) => {
-                                let response = "+PONG\r\n";
-
-                                stream.write_all(response.as_bytes()).unwrap();
-                                stream.flush().unwrap();
-                            },
-                            Err(_) => panic!("Error while reading command"),
-                        };
-                    }
+                Ok(stream) => {
+                    Redis::handle_connection(stream);
                 }
                 Err(e) => {
-                    println!("error: {}", e);
+                    println!("Error while processing stream: {}", e);
                 }
             }
         }
     }
+
+    fn handle_connection(mut stream: TcpStream) {
+        let mut command = [0; 512];
+
+        loop {
+            let read = stream.read(&mut command).unwrap();
+
+            if read == 0 {
+                break;
+            } else {
+                stream.write(b"+PONG\r\n").unwrap();
+            }
+
+        }
+    }
 }
+
+//  echo -e "PING\nPING" | ./spawn_redis_server.sh
